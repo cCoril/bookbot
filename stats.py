07ca41350c944d5e386e5865ac3bef388a9658e9
cCoril <carl.resmondo@gmail.com>
2025-06-27 04:37:10 -0400

    removed non-alpha characters

--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-test = "/home/carlr/workspace/github.com/cCoril/bookbot/books/frankenstein.txt"
+test = "books/frankenstein.txt"
 
 def get_num_words(file_path):
     with open(file_path) as f:
@@ -9,36 +9,43 @@ def get_num_words(file_path):
         counter += 1
     return f"{counter} words found in the document"
 
+
+
 def get_num_chars(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     words = file_contents.lower()
     count = {}
     for char in words:
-        if char in count:
-            count[char] += 1
-        else:
-            count[char] = 1
+        if char.isalpha():
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
     return count
+
+
 
 def get_org_dict(file_path):
     dict = get_num_chars(file_path)
     c_list = []
     for char in dict:
         num = dict[char]
-        result = {"char": char, "num": num}
+        result = {"char": f"{char}", "num": num}
         c_list.append(result)
     return c_list
+
 
 def sort_on(chars):
     return chars["num"]
 
+
 def sort_list(file_path):
     chars = get_org_dict(file_path)
-
     chars.sort(reverse=True, key=sort_on)
-    print(chars)
+    for group in chars:
+        result = print(group)
+    return result
 
-print(get_org_dict(test))
+print(sort_list(test))
 
-# result = {f"char:{char}, num: {num}"}
